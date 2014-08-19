6ab372ac50253358b683f1bc9c8241243e80abf1
SiteDisks <peter@sitedisks.com.au>
2014-08-19 15:18:01 +1000

    Add Marco Melbourne
    Marco Melbouren project
    Route config shortcut

--- SiteDisksNET/App_Start/RouteConfig.cs
+++ SiteDisksNET/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace SiteDisksNET
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "ShortcutRoute",
+                url: "{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
