a37dc712a0cf4d835e9942bed6827f1ea736d9db
SiteDisks <peter@sitedisks.com.au>
2014-08-27 17:00:54 +1000

    Adding Mock Testing
    Mock MOQ Nunit Testing

--- SiteDisksNET.Test/Properties/AssemblyInfo.cs
+++ SiteDisksNET.Test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SiteDisksNET.Tests")]
+[assembly: AssemblyTitle("SiteDisksNET.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SiteDisksNET.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("微软中国")]
+[assembly: AssemblyProduct("SiteDisksNET.Test")]
+[assembly: AssemblyCopyright("Copyright © 微软中国 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b35cc056-a6a5-4269-a349-2781966f2f42")]
+[assembly: Guid("6b1cbc80-c5d2-4040-b1e9-ce1a2dbed1a8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SiteDisksNET.Test/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SiteDisksNET.Models;
+using SiteDisksNET.Controllers;
+using Moq;
+using System.Collections.Generic;
+
+namespace SiteDisksNET.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {   // kakimotonline.com/2011/02/13/tdd-in-asp-net-mvc-applications-with-moq-framework/
+        
+        [TestMethod]
+        public void TestWebAPI()
+        {
+            Mock<IRepository<Contact>> _contactRepository = new Mock<IRepository<Contact>>();
+            var contacts = new List<Contact>();
+            contacts.Add(new Contact() { Id = 1, Name = "alpha", Email = "alpha@test.com", Subject = "Alpha Test", Message = "Alpha Message" });
+            contacts.Add(new Contact() { Id = 2, Name = "beta", Email = "beta@test.com", Subject = "Beta Test", Message = "Beta Message" });
+            contacts.Add(new Contact() { Id = 3, Name = "omega", Email = "omega@test.com", Subject = "Omega Test", Message = "Omega Message" });
+            _contactRepository.Setup(x => x.GetAll()).Returns(contacts);
+
+            var theController = new ContactsController(_contactRepository.Object);
+
+            var result = theController.GetContacts() as List<Contact>;
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual("beta", result[1].Name);
+            
+        }
+    }
+}
